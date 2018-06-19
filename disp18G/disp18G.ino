7a26a5e053235483a1f637810346881e6357f766
tockn <s.taku.inthebox@gmail.com>
2018-06-19 21:51:35 +0900

    [disp18G] add warning buzzer function and refactoring

--- disp18G/disp18G.ino
+++ disp18G/disp18G.ino
@@ -1,38 +1,58 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
-SoftwareSerial logger(10, 11);
+Servo yawServo;
 
 char recv;
-
 char r, p, y;
 
+long yawServoAngle;
+
+bool buzz = false;
+long buzzTimer = 0;
+
 void setup() {
-  logger.begin(9600);
   Serial.begin(9600);
   Serial.println("launch");
+  yawServo.attach(6);
 }
 
 void loop() {
-  while (logger.available() >= 12) {
-    if(header('r')) r = logger.read();
-    else  continue;
-    if(header('p')) p = logger.read();
-    else  continue;
-    if(header('y')) y = logger.read();
-    else  continue;
-    Serial.print((int)r); Serial.print("\t");
-    Serial.print((int)p); Serial.print("\t");
-    Serial.print((int)y); Serial.println();
+  if (Serial.available() >= 12) {
+    if(header('r')) r = (char)Serial.read();
+    if(header('p')) p = (char)Serial.read();
+    if(header('y')) y = (char)Serial.read();
+  }
+  yawServoAngle = map(y, -90, 90, 0, 180);
+  if (yawServoAngle > 180){
+    yawServoAngle = 180;
+  }
+  if (yawServoAngle < 0) {
+    yawServoAngle = 0;
+  }
+  yawServo.write(yawServoAngle);
+  Serial.println((int)y);
+  if (y > 45 || y < -45) {
+    buzz = true;
+  } else {
+    buzz = false;
+  }
+
+  if (buzz == true) {
+    if (millis() - buzzTimer > 400) {
+      tone(3, 2349, 300);
+      buzzTimer = millis();
+    }
   }
 } 
 
 bool header(char c) {
   char cc;
-  cc = (char)logger.read();
+  cc = (char)Serial.read();
   if (cc == c) {
-    cc = (char)logger.read();
+    cc = (char)Serial.read();
     if (cc == c) {
-      cc = (char)logger.read();
+      cc = (char)Serial.read();
       if (cc == c) {
         return true;
       } else {
